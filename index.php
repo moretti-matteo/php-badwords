be1bf79eada30ccbf9c2cc54b733871f88fc614d
moretti-matteo <matteomoretti2000@hotmail.it>
2022-06-21 13:56:51 +0200

    commit

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
-<?php 
+<?php
 
-    
+// Creare una variabile con un paragrafo di testo a vostra scelta.
+// Stampare a schermo il paragrafo e la sua lunghezza.
+// Una parola da censurare viene passata dall'utente tramite parametro GET.
+// Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+
+$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+
+$parola = $_GET['parola'];
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        div {
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
+
 <body>
+    <div><?php echo $text; ?></div>
+    <div>Lunghezza:<?php echo strlen($text); ?></div>
     
+    
+    
+    <div><?php echo str_replace($parola,'***',$text); ?></div>
+    <div>Lunghezza:<?php echo strlen($text);  ?></div>
+
 </body>
+
 </html>
